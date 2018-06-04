f49afaa323e94a44db1d816813bd62c0c54e7e2b
noribang <hoonk9387@gmail.com>
2018-06-03 19:52:56 -0500

    Changed React code in index.js to create new h1 heading in index.html.

--- react-help-queue/src/index.js
+++ react-help-queue/src/index.js
@@ -1,17 +1,7 @@
-function liveTime() {
-	var greeting = React.createElement('h1', {}, 'Hello, World!');
-	var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
-	var app = React.createElement('div', {}, greeting, clock);
+var heading = React.createElement('h1', {}, 'Help Queue');
+var app = React.createElement('div', {}, heading);
 
-	// ReactDOM.render(
-	// 	greeting, document.getElementById('react-app-root')
-	// );
-	ReactDOM.render(
-		app, 
-		document.getElementById('react-app-root')
-		);	
-}
-
-// setInterval will call liveTime() every second.
-// var clock re-defined everytime liveTime() is called.
-setInterval(liveTime, 1000);
+ReactDOM.render(
+	app,
+	document.getElementById('react-app-root')
+	);
